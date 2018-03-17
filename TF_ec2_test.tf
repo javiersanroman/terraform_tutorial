56c91f52165a4c7a5ee04346697a97c58948df7e
jsr <javier.sanroman@zocobox.com>
2018-03-17 19:55:30 +0100

    Initial commit

---
+++ TF_ec2_test.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+	access_key="AKIAIF3557ZLZXHNJHNA"
+	secret_key="qQDqMhFxNYHkmOVqtNZ5BMWvg+H4lrtKdlqcu9Uz"
+	region="eu-central-1"
+}
+
+resource "aws_instance" "jsr_example" {
+	#centos 7 official AMI (paid subscription)
+	ami = "ami-337be65c"  
+	instance_type = "t2.micro"
+}
