507e430fee854c50b5a74c6f94c827b0e955a2d9
jsr <javier.sanroman@zocobox.com>
2018-03-18 22:13:04 +0100

    Small changes

--- TF_ec2_test.tf
+++ TF_ec2_test.tf
@@ -1,11 +1,10 @@
 provider "aws"{
-	access_key="AKIAIF3557ZLZXHNJHNA"
-	secret_key="qQDqMhFxNYHkmOVqtNZ5BMWvg+H4lrtKdlqcu9Uz"
+	shared_credentials_file = "./.aws/credentials"
 	region="eu-central-1"
 }
 
 resource "aws_instance" "jsr_example" {
 	#centos 7 official AMI (paid subscription)
-	ami = "ami-337be65c"  
+	ami = "ami-5055cd3f"
 	instance_type = "t2.micro"
 }
